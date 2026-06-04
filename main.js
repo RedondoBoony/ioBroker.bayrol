3fb3b23e9638cbb0c363b950230f32637fc2a3ff
Sven <sven@rutten.ch>
2026-06-04 17:02:52 +0200

    feat: puppeteer scraper fuer mail.rutten.ch, alle 10 Sekunden neueste Mail loggen

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 'use strict';
 
 const utils = require('@iobroker/adapter-core');
+const puppeteer = require('puppeteer');
 
 class Bayrol extends utils.Adapter {
 
-    /**
-     * @param {Partial<utils.AdapterOptions>} [options={}]
-     */
     constructor(options) {
         super({
             ...options,
@@ -14,34 +12,87 @@ class Bayrol extends utils.Adapter {
         });
 
         this.interval = null;
+        this.browser = null;
 
         this.on('ready', this.onReady.bind(this));
         this.on('unload', this.onUnload.bind(this));
     }
 
-    /**
-     * Is called when databases are connected and adapter received configuration.
-     */
+    async scrapeLatestMail() {
+        const username = this.config.username;
+        const password = this.config.password;
+
+        let browser = null;
+        try {
+            browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            });
+            const page = await browser.newPage();
+
+            // Login
+            await page.goto('https://mail.rutten.ch/', { waitUntil: 'networkidle2' });
+            await page.type('input[name="username"]', username);
+            await page.type('input[name="password"]', password);
+            await Promise.all([
+                page.waitForNavigation({ waitUntil: 'networkidle2' }),
+                page.click('button[type="submit"], input[type="submit"]'),
+            ]);
+
+            // Warte auf Weiterleitung zur Inbox (SOGo oder ähnlich)
+            await page.waitForTimeout(3000);
+
+            const currentUrl = page.url();
+            this.log.debug(`Nach Login URL: ${currentUrl}`);
+
+            // Versuche den neuesten Mail-Betreff zu finden
+            const subject = await page.evaluate(() => {
+                // Allgemeine Selektoren für Mailcow / SOGo
+                const selectors = [
+                    '.sg-mail-subject',
+                    '.messageSubject',
+                    '[class*="subject"]',
+                    '[class*="Subject"]',
+                    'td.subject',
+                ];
+                for (const sel of selectors) {
+                    const el = document.querySelector(sel);
+                    if (el && el.innerText) return el.innerText.trim();
+                }
+                return null;
+            });
+
+            if (subject) {
+                this.log.info(`Neueste Mail Betreff: ${subject}`);
+            } else {
+                this.log.warn('Konnte keinen Mail-Betreff finden. Seiten-Struktur eventuell abweichend.');
+            }
+
+        } catch (err) {
+            this.log.error(`Scraping-Fehler: ${err.message}`);
+        } finally {
+            if (browser) await browser.close();
+        }
+    }
+
     async onReady() {
         const username = this.config.username;
+        const password = this.config.password;
 
-        if (!username) {
-            this.log.warn('Keidn Befnutzername konfiguriert. Bitte in den Adaptereinstellungen eintragen.');
+        if (!username || !password) {
+            this.log.warn('Benutzername oder Passwort nicht konfiguriert. Bitte in den Adaptereinstellungen eintragen.');
             return;
         }
 
-        this.log.info(`Adapter gestartet. Benutzername: ${username}`);
+        this.log.info(`Adapter gestartet. Starte Scraping für: ${username}`);
 
-        // Log den Benutzernamen alle 5 Sekunden
+        // Sofort starten, dann alle 10 Sekunden
+        await this.scrapeLatestMail();
         this.interval = setInterval(() => {
-            this.log.info(`Benutzername: ${username}`);
-        }, 5000);
+            this.scrapeLatestMail();
+        }, 10000);
     }
 
-    /**
-     * Is called when adapter shuts down - callback has to be called under any circumstances!
-     * @param {() => void} callback
-     */
     onUnload(callback) {
         try {
             if (this.interval) {
